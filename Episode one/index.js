b47fc60c0ff76ae29bf2f38f2e68a7345e29db88
Collbrothers <aron@kyleback.com>
2020-09-26 05:37:08 +0200

    init

---
+++ Episode one/index.js
@@ -0,0 +1,35 @@
+const { Client, MessageEmbed } = require("discord.js"); // Require the required files
+const client = new Client();
+
+client.on("ready", () => {
+  console.log(`${client.user.username} just went online!`); // Logs the name of the client saying it's online when the ready event get's "emitted"
+  client.user.setPresence({
+    activity: {
+      name: "a tutorial about discord.js!",
+      type: "WATCHING", // Can be: "PLAYING", "STREAMING", "LISTENING" or WATCHING of course!
+    },
+    status: "online", // Can be: "dnd", "invisible" (offline), "idle" or "online"
+  });
+});
+// Listen for the message event, this block of code will run the the client "emits" the event, in easier words, it runs when a message gets sent!
+client.on("message", (message) => {
+  // The (message) argument can be named anything you'd like, but I would recommend keeping it to subject, but some recommended options: (msg), (message), (m)
+  if (message.content.toUpperCase() === "?PING") {
+    // Be sure to always set the string characters to uppercase, else this would always return false.
+    message.reply("Pong!"); // @YouUsername, Pong!
+    message.channel.send("Pong!"); // Pong!
+  }
+  if (message.content.toUpperCase() === "?ABOUT") {
+    const embed = new MessageEmbed() // Initiate a MessageEmbed class, giving you access to methods like setTitle, addField etc
+      .setTitle("About")
+      .setDescription("Here is some useful information about me!")
+      .setThumbnail(client.user.displayAvatarURL())
+      .addField("Commands", "Ping\nAbout") // \n will switch line, so About will be one line under Ping
+      .addField("Prefix", "?") // Prefix is the name, and ? is the value of this field, good to keep in mind!
+      .addField("Created at", client.user.createdAt) // client#user#createdAt returns a date object, so you can modify it easily, see more here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+      .setColor("RED"); // Color can be either hex, rgb (array) or the deafult colors, see them here: https://discord.js.org/#/docs/main/stable/typedef/ColorResolvable
+    message.channel.send(embed); // send the embed to the channel
+  }
+});
+
+client.login("Your token goes here"); // find your token  at https://discord.com/developers/applications under an application -> Bot -> Token
---
+++ Episode one/readme.md
@@ -0,0 +1,32 @@
+# Episode 1
+
+### Stuff we went through in this episode:
+
+```
+Downloading Nodejs, NPM and Visual Studio Code
+Basic NPM, Nodejs commands
+Creating a new discord Application and adding a bot to it
+Inviting the bot to a server
+Introduction to EventListeners (Discord.js Client)
+Logging on ready, and message event
+Setting a presence
+Replying and send messages to a channel when a specific prefix and command has been sent
+Some basics about MessageEmbeds methods
+Login/start the client
+```
+
+### Stuff to keep in mind:
+
+```
+Try to keep the presence updating in the ready event, else you could get ratelimited
+A field in a MessageEmbed requires both a name and a value: embed.addField("name","value")
+Try to keep the main entry point named index.js
+Try to keep the variable names consistent with the value it holds
+The bot token is like a password for the bot; Do not share it with people!
+To run a JS file, type: node <Filename, or .>
+To install a package, simply type: npm <i or install> <packageName>
+```
+
+### Creator(s)
+
+#### Collbrothers (Voice and code)
